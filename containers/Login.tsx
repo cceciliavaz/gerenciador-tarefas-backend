6c499931edcd52e0ebde90d81dea97814cea9b98
Ana Cecília Vaz <73395116+cceciliavaz@users.noreply.github.com>
2022-08-01 23:04:31 -0300

    :sparkles: (login) criando login e estilização

---
+++ containers/Login.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+export const Login = () => {
+  return (
+    <div className="container-login">
+      <img src="/logo.svg" className="logo" alt="Logo da FIAP" />
+      <form>
+        <div className="input">
+          <img src="/mail.svg" alt="Informe o seu login" />
+          <input type="text" placeholder="Login"></input>
+        </div>
+        <div className="input">
+          <img src="/lock.svg" alt="Informe a sua senha" />
+          <input type="password" placeholder="Senha"></input>
+        </div>
+        <button> Login </button>
+      </form>
+    </div>
+  );
+};
